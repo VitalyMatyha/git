dbdead4198ec377bbff41194a44260760d7cf5b1
Vitaly <vitaly-1910@mail.ru>
2025-03-18 22:36:21 +0300

    homework9.1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from src.masks import get_mask_card_number
-
+from src.masks import get_mask_account
 print(get_mask_card_number(1934567386528468))
+print(get_mask_account(1934567386528468))
---
+++ src/widget/widget.py
@@ -0,0 +1,11 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Возвращает маску номера счета в формате XXXX XX** **** XXXX"""
+    # Преобразуем номер карты в строку
+    card_str = str(card_number)
+    # Проверяем, достаточно ли длинный номер
+    if len(card_str) == 16 and card_str.isdigit():
+        # Создаем маску, оставляя видимыми только последние 4 цифры
+        masked_card = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[12:]}"
+        return masked_card
+    else:
+        return "Ошибка"
