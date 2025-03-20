6563034078c9253c4341c1492ebd439e60dd7e39
Vitaly <vitaly-1910@mail.ru>
2025-03-20 20:49:16 +0300

    справил main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.masks import get_mask_card_number
 from src.masks import get_mask_account
+from src.masks import get_mask_card_number
+
 print(get_mask_card_number(1934567386528468))
 print(get_mask_account(1934567386528468))
