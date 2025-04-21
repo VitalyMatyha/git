f8214c71daa54e0dffb6b17cec04d5c3a756a471
Vitaly <vitaly-1910@mail.ru>
2025-04-21 14:31:39 +0300

    add new func generators

---
+++ src/generators.py
@@ -0,0 +1,17 @@
+from typing import Iterator, List, Dict
+
+
+def filter_by_currency(transactions: List[Dict], currency_code: str) -> Iterator[Dict]:
+    for transaction in transactions:
+        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency_code:
+            yield transaction
+
+
+def transaction_descriptions(transactions: List[Dict]) -> Iterator[str]:
+    for transaction in transactions:
+        yield transaction.get("description", "")
+
+
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
+    for number in range(start, stop + 1):
+        yield f"{number:016d}"[:4] + " " + f"{number:016d}"[4:8] + " " + f"{number:016d}"[8:12] + " " + f"{number:016d}"[12:16]
