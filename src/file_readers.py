6b137205feb92bc0fe9f1805167d4639d612a6f9
Vitaly <vitaly-1910@mail.ru>
2025-06-02 20:13:31 +0300

    add func read_file

---
+++ src/file_readers.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from typing import List, Dict
+
+
+def read_transactions_from_csv(transactions: str) -> List[Dict]:
+    """
+    Читает CSV-файл и возвращает список транзакций.
+
+    Args:
+        filepath (str): Путь к CSV-файлу.
+
+    Returns:
+        List[Dict]: Список транзакций.
+    """
+    try:
+        df = pd.read_csv(transactions)
+        return df.to_dict(orient='records')
+    except Exception as e:
+        return []
+
+
+def read_transactions_from_excel(transactions_excel: str) -> List[Dict]:
+    """
+    Читает Excel-файл и возвращает список транзакций.
+
+    Args:
+        filepath (str): Путь к Excel-файлу (.xlsx).
+
+    Returns:
+        List[Dict]: Список транзакций.
+    """
+    try:
+        df = pd.read_excel(transactions_excel)
+        return df.to_dict(orient='records')
+    except Exception as e:
+        return []
